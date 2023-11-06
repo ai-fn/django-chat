8b5cc4cf3333a49c9c7c918b072ddfbe82f927f8
zhirov <zhirov_andrei02@list.ru>
2023-11-06 16:46:42 +0300

    Reduce js code, create the ability for choose room image

--- static/js/rooms.js
+++ static/js/rooms.js
@@ -1,30 +1,13 @@
-const modal = document.getElementsByClassName('modal')
-function createRoom () {
-    const roomName = document.getElementById('room-name-inp').value
-    const roomImage = document.getElementById('room-image-inp').value
-    const roomImageFile = document.getElementById('room-image-inp').files[0]
-    const acceptedExt = ['jpg', 'png', 'jpeg']
-    const roomImgSplit = roomImage.split('.')
-    const roomImgExt = roomImgSplit[roomImgSplit.length - 1]
-    let formData = new FormData()
-    const request = new XMLHttpRequest()
+try{
+    document.getElementById('imgInput').addEventListener('change', function(event) {
 
-    if (roomName === ""){
-        console.log(roomName === "")
-        alert('Type room name!')
-        return;
+    const file = event.target.files[0];
+    const reader = new FileReader();
+  
+    reader.onload = function(e) {
+      document.getElementById('roomImgPrev').style.backgroundImage = `url(${e.target.result})`;
     }
-    request.open("POST", "create-chat/", true)
-    request.withCredentials = true
-    formData.append('room-name', roomName)
-    formData.append('csrfmiddlewaretoken', "{{ csrf_token }}")
-
-    if (acceptedExt.some(ext => ext == roomImgExt)) {
-        formData.append('room-image', roomImageFile)
-    }
-
-    request.send(formData)
-    console.log(request.status)
-    modal.style.display = "none"
-    $.get('http://127.0.0.1:8000/chats/')
-}
+  
+    reader.readAsDataURL(file);
+  });
+} catch {console.log("error")}
