44192d04d6ef603cc3d6b9bf2f4d71824de13d13
zhirov <zhirov_andrei02@list.ru>
2023-12-27 17:43:51 +0300

    attachModal initialize

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,3 +1,4 @@
+import { createAttachModal, displayImage } from "./attachModal.js";
 import { editableMessageText } from "./editMessage.js"; 
 
 let attachButton = document.getElementById('attach-menu-button');
@@ -18,7 +19,7 @@ attachButton.addEventListener("mouseenter", () => {
 });
 
 menuContainer.addEventListener("mouseleave", () => {
-    menuContainer.classList.remove('activated');
+    attachButton.classList.remove('activated');
     attachControls.getElementsByClassName('backdrop')[0].remove();
     menuContainer.classList.replace('open', 'not-open');
     menuContainer.classList.replace('shown', 'not-shown');
@@ -33,27 +34,53 @@ fileInput.addEventListener('click', () => {
 });
 
 function createFileInput({allowedExt}) {
-    const inp = document.createElement("input");
+    let inp = document.createElement("input");
+    let formData;
+	let files;
+	if (localStorage.getItem('formData') != null){
+		formData = JSON.parse(localStorage.formData);
+		files = JSON.parse(formData.files);
+	} else {
+		formData = new FormData();
+		formData.append("files", files);
+	}
     inp.type = "file";
-    inp.accept = allowedExt;
+	inp.accept = allowedExt;
     inp.style.display = "none";
     document.body.appendChild(inp);
 
     inp.addEventListener("change", (e) => {
-        const file = e.target.files[0];
-        if (!file) return;
-    
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          const fileName = file.name;
-          const base64String = String.fromCharCode.apply(null, new Uint8Array(reader.result))
-          localStorage.attachFileName = fileName
-          localStorage.attachFile = base64String;
-        };
-        reader.readAsArrayBuffer(file);
-    
-        document.body.removeChild(inp);
-      });
-    localStorage.caption = editableMessageText.innerText;
-    inp.click();
+			if (document.getElementsByClassName("Modal")[0] == undefined){
+				createAttachModal();
+			}
+			const selectedFiles = e.target.files;
+			if (!selectedFiles) return;
+	
+			for (var i = 0; i < selectedFiles.length; i++) {
+				let file = selectedFiles[i];
+				const reader = new FileReader();
+				reader.onload = (e) => {
+					let prevFiles;
+					let newFile = JSON.stringify({
+						name: file.name,
+						base64String: String.fromCharCode.apply(null, new Uint8Array(reader.result))
+					})
+					displayImage(e.target.result);
+					if (files != undefined){
+						prevFiles = JSON.parse(files);
+						prevFiles.files.push(newFile)
+					} else {
+						prevFiles = JSON.stringify({
+							files: new Array(newFile),
+						});
+					}
+					formData.files = JSON.stringify(prevFiles);
+					localStorage.formData = JSON.stringify(formData)
+				};
+				reader.readAsDataURL(file);
+				document.body.removeChild(inp);
+			}
+	});
+	localStorage.caption = editableMessageText.innerText;
+	inp.click();
 }
