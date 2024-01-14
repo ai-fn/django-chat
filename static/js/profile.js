7f6c24e9daa8d37a2c129e5162cf1f4510badd1b
zhirov <zhirov_andrei02@list.ru>
2024-01-14 12:36:56 +0300

    update user Avatar preview

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,13 +1,16 @@
 try{
     document.getElementById('imageInput').addEventListener('change', function(event) {
+try {
+  document.querySelector('.AvatarEditable input').addEventListener('change', function (event) {
 
     const file = event.target.files[0];
     const reader = new FileReader();
-  
-    reader.onload = function(e) {
-      document.getElementById('avatarImage').style.backgroundImage = `url(${e.target.result})`;
+
+    reader.onload = function (e) {
+      document.querySelector('.AvatarEditable img').src = `${e.target.result}`;
     }
-  
+
     reader.readAsDataURL(file);
+    inputHandler();
   });
-} catch {console.log("error")}
+} catch { console.log("error") }
