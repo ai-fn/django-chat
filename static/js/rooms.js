13efe6af467abddf460bb78e7b68414f2bd40945
zhirov <zhirov_andrei02@list.ru>
2024-01-14 12:35:42 +0300

    update room image preview on input

--- static/js/rooms.js
+++ static/js/rooms.js
@@ -1,13 +1,29 @@
 try{
     document.getElementById('imgInput').addEventListener('change', function(event) {
+roomNameInp.addEventListener("input", () => {
+  if (!img.src){
+    roomImgPrevContainer.innerHTML = roomNameInp.value ? roomNameInp.value[0].toUpperCase() : "-";
+  }
+})
+
+for (let el of folders){
+  el.addEventListener("click", scrollInto);
+}
+
+try {
+  roomImgInput.addEventListener('change', function(event) {
 
     const file = event.target.files[0];
     const reader = new FileReader();
+
+    roomImgPrevContainer.parentElement.classList.remove("no-photo", "peer-color-3");
   
     reader.onload = function(e) {
-      document.getElementById('roomImgPrev').style.backgroundImage = `url(${e.target.result})`;
+      img.src = e.target.result;
+      roomImgPrevContainer.innerHTML = "";
+      roomImgPrevContainer.insertAdjacentElement("afterbegin", img);
     }
-  
+
     reader.readAsDataURL(file);
   });
 } catch(error) {
