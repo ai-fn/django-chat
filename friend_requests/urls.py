e2c0b1a80d179b6962d990b44c7e34287f8723b8
zhirov <zhirov_andrei02@list.ru>
2024-01-17 22:04:26 +0300

    [FIX] bug with url

--- friend_requests/urls.py
+++ friend_requests/urls.py
@@ -5,7 +5,7 @@ app_name = 'friend_requests'
 
 urlpatterns = [
     path('friend-requests/', views.RequestList.as_view(), name='list'),
-    path('friend-requests/<int:req_id>/', views.FriendRequestView, name='view'),
+    path('friend-requests/<int:req_id>/', views.FriendRequestView.as_view(), name='view'),
     path('friend-requests/set_as_accepted/<int:request_pk>/', views.RequestSetAsAccepted.as_view(), name='set_as_accepted'),
     path('friend-requests/set_as_declined/<int:request_pk>/', views.RequestSetAsDeclined.as_view(), name='set_as_declined'),
     path('friend-requests/send-request/<int:to_user_pk>/', views.SendRequest.as_view(), name='send'),
